b6f4ecdd00f21f485bface383c9c623902ce7b9e
0nyx-hkr <76675355+0nyx-hkr@users.noreply.github.com>
2021-11-29 20:31:34 +0530

    Update morse.py

--- morse.py
+++ morse.py
@@ -1,1 +1,153 @@
-import sys
+import time
+import 
+
+class Morse:
+	def __init__(self,letter)
+	self.letter = letter
+		
+	#def dot():
+	
+	#def dash():
+	
+	def a():
+		dot()
+		dash()
+		time.sleep(0.8)
+	def b():
+			dash()
+			dot()
+			dot()
+			dot()
+			time.sleep(0.8)
+	def c():
+			dash()
+			dot()
+			dash()
+			dot()
+			time.sleep(0.8)
+			
+	def d():
+			dash()
+			dot()
+			dot()
+			time.sleep(0.8)
+	def e():
+			dot()
+			time.sleep(0.8)
+	def f():
+			dot()
+			dot()
+			dash()
+			dot()
+			time.sleep(0.8)
+	def g():
+			dash()
+			dash()
+			dot()
+			time.sleep(0.8)
+	def h():
+			dot()
+			dot()
+			dot()
+			dot()
+			time.sleep(0.8)
+	def i():
+			dot()
+			dot()
+			time.sleep(0.8)
+	def j():
+			dot()
+			dash()
+			dash()
+			dash()
+			time.sleep(0.8)
+	def k():
+			dash()
+			dot()
+			dash()
+			time.sleep(0.8)
+	def l():
+			dot()
+			dash()
+			dot()
+			dot()
+			time.sleep(0.8)
+	def m():
+			dash()
+			dash()
+			time.sleep(0.8)
+			
+	def n():
+			dash()
+			dot()
+			time.sleep(0.8)
+			
+	def o():
+			dash()
+			dash()
+			dash()
+			time.sleep(0.8)
+	def p():
+			dot()
+			dash()
+			dash()
+			dot()
+			time.sleep(0.8)
+			
+	def q():
+			dash()
+			dash()
+			dot()
+			dash()
+			time.sleep(0.8)
+	def r():
+			dot()
+			dash()
+			dot()
+			time.sleep(0.8)
+	def s():
+			dot()
+			dot()
+			dot()
+			time.sleep(0.8)
+	def t():
+			dash()
+			time.sleep(0.8)
+	def u():
+			dot()
+			dot()
+			dash()
+			time.sleep(0.8)
+			
+	def v():
+			dot()
+			dot()
+			dot()
+			dash()
+			time.sleep(0.8)
+	def w():
+			dot()
+			dash()
+			dash()
+			time.sleep(0.8)
+	def x():
+			dash()
+			dot()
+			dot()
+			dash()
+			time.sleep(0.8)
+	def y():
+			dash()
+			dot()
+			dash()
+			dash()
+			time.sleep(0.8)
+	def z():
+			dash()
+			dash()
+			dot()
+			dot()
+			time.sleep(0.8)
+		
+				
+
